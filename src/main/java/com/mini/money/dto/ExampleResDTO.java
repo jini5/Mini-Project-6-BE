108364400e9830158aa978a04a63172e08001219
DongHyos <dong87070@naver.com>
2023-02-10 01:38:24 +0900

    #3 - 프로젝트 구조 잡기
    스프링 프로젝트에 맞게 구조를 잡고 Example 파일들을 추가. 추후 삭제 예정

---
+++ src/main/java/com/mini/money/dto/ExampleResDTO.java
@@ -0,0 +1,22 @@
+//package com.mini.money.dto;
+//
+//import lombok.*;
+//
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@ToString
+//public class ExampleResDTO {
+//    private String userId;
+//    private String username;
+//    private String nickname;
+//    private String token;
+//
+//    public LoginResponse(Account acc, String token) {
+//        this.userId = acc.getUserId();
+//        this.username = acc.getUsername();
+//        this.nickname = acc.getNickname();
+//        this.token = token;
+//    }
+//}
---
+++ src/main/java/com/mini/money/repository/ExampleRepository.java
@@ -0,0 +1,5 @@
+//package com.mini.money.repository;
+//
+//public interface ExampleRepository JpaRepository<Account, String>{
+//        Optional<Account> findByUserId(String userId);
+//}
