c48d964e6fc132c515bb755b3effcaf11f672823
DongHyos <dong87070@naver.com>
2023-02-13 17:29:08 +0900

    #9 - 데이터 통합을 위한 API 코드 및 Repository
    데이터 통합을 위해 클래스 명 테이블 수정

--- src/main/java/com/mini/money/repository/LoanDetailRepository.java
+++ src/main/java/com/mini/money/repository/LoanDetailRepository.java
@@ -1,9 +1,7 @@
 package com.mini.money.repository;
 
-import com.mini.money.entity.HomeLoanDetail;
+import com.mini.money.entity.LoanDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface HomeLoanDetailRepository extends JpaRepository<HomeLoanDetail, Long> {
+public interface LoanDetailRepository extends JpaRepository<LoanDetail, Long> {
 }
--- src/main/java/com/mini/money/repository/LoanRepository.java
+++ src/main/java/com/mini/money/repository/LoanRepository.java
@@ -2,8 +2,6 @@ package com.mini.money.repository;
 
 import com.mini.money.entity.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface HomeLoanRepository extends JpaRepository<Loan, Long> {
+public interface LoanRepository extends JpaRepository<Loan, Long> {
 }
