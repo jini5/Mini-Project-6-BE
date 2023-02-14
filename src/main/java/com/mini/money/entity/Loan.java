ea79b773135d272c5a39a53d149961b0a5f48021
DongHyos <dong87070@naver.com>
2023-02-14 21:49:32 +0900

    #9 - entity 수정
    - 직관적인 명명
    
    - Customer 테이블에 role 추가

--- src/main/java/com/mini/money/entity/Loan.java
+++ src/main/java/com/mini/money/entity/Loan.java
@@ -20,95 +20,95 @@ public class Loan {
     private Long snq;
 
     @Column(name = "bas_ym")
-    private String basYm;
+    private String baseDate; // 기준년월
 
     @Column(name = "fin_prd_nm")
-    private String finPrdNm;
+    private String loanName; // 금융상품명
 
     @Column(name = "ln_lmt")
-    private String lnLmt;
+    private String loanLimit; // 대출한도
 
     @Column(name = "irt_ctg")
-    private String irtCtg;
+    private String baseRate; // 금리구분
 
     @Column(name = "irt")
-    private String irt;
+    private String rate; // 금리
 
     @Column(name = "max_tot_ln_trm")
-    private String maxTotLnTrm;
+    private String wholePeriod; // 최대총대출기간
 
     @Column(name = "max_dfrm_trm")
-    private String maxDfrmTrm;
+    private String gracePeriod; // 최대거치기간
 
     @Column(name = "max_rdpt_trm")
-    private String maxRdptTrm;
+    private String repayPeriod; // 최대상환기관
 
     @Column(name = "rdpt_mthd")
-    private String rdptMthd;
+    private String repayMethod; // 상환방법
 
     @Column(name = "usge")
-    private String usge;
+    private String usge; // 용도
 
     @Column(name = "trgt")
-    private String trgt;
+    private String loanTarget; // 대상
 
     @Column(name = "inst_ctg")
-    private String instCtg;
+    private String divisionOffice; // 기관구분
 
     @Column(name = "ofr_inst_nm")
-    private String ofrInstNm;
+    private String provider; // 제공기관명
 
     @Column(name = "rsd_area_pamt_eglt_istm")
-    private String rsdAreaPamtEqltIstm;
+    private String area; // 거주지역원금균등분할상환
 
     @Column(name = "supr_tgt_dtl_cond")
-    private String suprTgtDtlCond;
+    private String loanDescription; // 지원대상 상세조건
 
     @Column(name = "age")
-    private String age;
+    private String age; // 연령
 
     @Column(name = "incm")
-    private String incm;
+    private String income; // 소득
 
     @Column(name = "crdt_sc")
-    private String crdtSc;
+    private String creditScore; // 신용등급
 
     @Column(name = "grn_inst")
-    private String grnInst;
+    private String trustOffice; // 보증기관
 
     @Column(name = "jn_mthd")
-    private String jnMthd;
+    private String joinMethod; // 가입(신청)방법
 
     @Column(name = "rpymd_cfe")
-    private String rpymdCfe;
+    private String earlyRedemptionFee; // 중도상환수수료
 
     @Column(name = "ln_icdcst")
-    private String lnIcdcst;
+    private String addCost; // 대출부대비용
 
     @Column(name = "ov_itr_yr")
-    private String ovItrYr;
+    private String overdueRate; // 연체이자율
 
     @Column(name = "prft_add_irt_cond")
-    private String prftAddIrtCond;
+    private String primeCondition; // 우대금리/가산금리 조건
 
     @Column(name = "etc_ref_sbjc")
-    private String etcRefSbjc;
+    private String etcNote; // 기타참고사항
 
     @Column(name = "hdl_inst")
-    private String hdlInst;
+    private String userOffice; // 취급기관
 
     @Column(name = "cnpl")
-    private String cnpl;
+    private String contact; // 연락처
 
     @Column(name = "rlt_site")
-    private String rltSite;
+    private String homepage; // 관련사이트
 
     @Column(name = "prd_nm")
-    private String prdNm;
+    private String loanClassification; // 상품 분류
 
     @Column(name = "mgm_dln")
-    private String mgmDln;
+    private String maturity; // 운영기한
 
     @Column(name = "prd_ctg2")
-    private String prdCtg2;
+    private String loanType; // 상품타입
 }
