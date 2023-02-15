57f17342bbb28fefec9e7dfed5eeb72872f85009
Blooossom <taegyeong19@gmail.com>
2023-02-15 20:04:10 +0900

    #34 LoanRepository 작성
    1. snq를 통한 Loan search

--- src/main/java/com/mini/money/repository/LoanRepository.java
+++ src/main/java/com/mini/money/repository/LoanRepository.java
@@ -3,5 +3,10 @@ package com.mini.money.repository;
 import com.mini.money.entity.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LoanRepository extends JpaRepository<Loan, Long> {
+
+
+    Optional<Loan> findBySnq(Long snq);
 }
