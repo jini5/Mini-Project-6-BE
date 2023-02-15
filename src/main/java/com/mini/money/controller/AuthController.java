2c21020f8688509cfefc1c4157c23e2785871757
jini5 <lim789789@gmail.com>
2023-02-16 01:34:13 +0900

    feat: AuthConroller 생성

---
+++ src/main/java/com/mini/money/controller/AuthController.java
@@ -0,0 +1,42 @@
+package com.mini.money.controller;
+
+import com.mini.money.dto.CustomerReqDTO;
+import com.mini.money.dto.LogInReqDTO;
+import com.mini.money.dto.LogInResDTO;
+import com.mini.money.entity.Blacklist;
+import com.mini.money.repository.TokenRepository;
+import com.mini.money.service.CustomerService;
+import com.mini.money.service.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final CustomerService customerService;
+    private final TokenService tokenService;
+
+
+    @PostMapping("/signup")
+    public String signUp(CustomerReqDTO signupReqDTO){
+        return customerService.signup(signupReqDTO);
+    }
+
+    @PostMapping("/login")
+    public LogInResDTO logIn(LogInReqDTO logInReqDTO){
+        return customerService.login(logInReqDTO);
+    }
+///https://fa466e8e-2ce8-447d-b2dd-517832056015.mock.pstmn.io
+    @PostMapping("/logout")
+    public String logOut(@RequestHeader(name = "Authorization") String token){
+        System.out.println(token);
+
+        tokenService.logout(token);
+        return "success";
+    }
+
+
+}
