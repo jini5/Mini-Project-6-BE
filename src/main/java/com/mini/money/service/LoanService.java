f52fa3965da409092f3568c4e6f8b2077a7edbcd
Blooossom <taegyeong19@gmail.com>
2023-02-15 14:52:41 +0900

    #22 LoanService Interface 작성
    1. 대출 리스트 조회 인터페이스 추가

---
+++ src/main/java/com/mini/money/service/LoanService.java
@@ -0,0 +1,12 @@
+package com.mini.money.service;
+
+import com.mini.money.dto.LoanResDTO;
+
+import java.util.List;
+
+public interface LoanService {
+
+    List<LoanResDTO> selectLoanList();
+
+
+}
