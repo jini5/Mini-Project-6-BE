4f07ac8c527c7138ec5a05b5c400eb0296d00b58
DongHyos <dong87070@naver.com>
2023-02-15 21:26:33 +0900

    #27 - 비즈니스 로직 구현
    - CustomerDetail 테이블에 Customer가 들어가 있다면 컬럼값이 null이든지 아니든지 전달
    - 단, Customer 테이블에는 있지만 CustomerDetail 테이블에 없다면 IllegalStateException 에러를 발생

--- src/main/java/com/mini/money/service/CustomerDetailService.java
+++ src/main/java/com/mini/money/service/CustomerDetailService.java
@@ -1,6 +1,7 @@
 package com.mini.money.service;
 
 import com.mini.money.dto.CustomerDetailReqDTO;
+import com.mini.money.dto.myinfo.MyCustomerDetailInfoResDTO;
 import com.mini.money.entity.Customer;
 import com.mini.money.entity.CustomerDetail;
 import com.mini.money.repository.CustomerDetailRepository;
@@ -21,5 +22,22 @@ public class CustomerDetailService {
         customerDetailRepository.save(customerDetail);
 
         return "success";
+    }
+
+    public MyCustomerDetailInfoResDTO findMyDetailInfo(String email) {
+        try {
+            Customer loginCustomer = customerRepository.findByEmail(email);
+            CustomerDetail customerDetail = customerDetailRepository.findByCustomer(loginCustomer).orElseThrow(() -> new IllegalStateException("추가 정보 등록 요망"));
+
+            MyCustomerDetailInfoResDTO my = new MyCustomerDetailInfoResDTO(
+                    customerDetail.getAge(), customerDetail.getAddress(), customerDetail.getJob(),
+                    customerDetail.getBank(), customerDetail.getCrdtGrade(), customerDetail.getIncome()
+            );
+
+            return my;
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
