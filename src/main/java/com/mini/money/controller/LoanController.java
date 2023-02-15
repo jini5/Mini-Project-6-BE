8ee5f337d49097acdfa52a2346fba4f5f2b541b4
Blooossom <taegyeong19@gmail.com>
2023-02-15 15:07:36 +0900

    #22 Add LoanController
    1. Loan Controller 작성(대출 리스트 조회 기능)

---
+++ src/main/java/com/mini/money/controller/LoanController.java
@@ -0,0 +1,23 @@
+package com.mini.money.controller;
+
+
+import com.mini.money.dto.LoanResDTO;
+import com.mini.money.service.LoanService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class LoanController {
+
+    private final LoanService service;
+
+    @GetMapping("/finance/loan")
+    public List<LoanResDTO> selectLoanList(){
+        return service.selectLoanList();
+    }
+    
+}
