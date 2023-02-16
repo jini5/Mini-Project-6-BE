59313fa3038f0fb9a112f258712f5eed6c2eb2a5
Blooossom <taegyeong19@gmail.com>
2023-02-16 14:17:22 +0900

    #36 CartRepository 작성
    1. deleteCustomerAndLoan
    1-1. Customer Entity와 Loan Entity를 통해 탐색하여 Cart삭제

--- src/main/java/com/mini/money/repository/CartRepository.java
+++ src/main/java/com/mini/money/repository/CartRepository.java
@@ -1,10 +1,17 @@
 package com.mini.money.repository;
 
 import com.mini.money.entity.Cart;
+import com.mini.money.entity.Customer;
+import com.mini.money.entity.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    void deleteByCustomerAndLoan(Customer customer, Loan loan);
+
 
 }
