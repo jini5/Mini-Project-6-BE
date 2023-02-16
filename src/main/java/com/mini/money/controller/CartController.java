350bc0708b7b4dc88db2ed18b9b927cd09a484fb
Blooossom <taegyeong19@gmail.com>
2023-02-16 15:55:10 +0900

    #35 Annotation 수정
    1. @PathVariable -> @RequestBody로 수정

--- src/main/java/com/mini/money/controller/CartController.java
+++ src/main/java/com/mini/money/controller/CartController.java
@@ -5,10 +5,7 @@ import com.mini.money.entity.Customer;
 import com.mini.money.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,12 +14,12 @@ public class CartController {
     private final CartService cartService;
 
     @PostMapping("/cart")
-    public String addCart(@PathVariable("snq") Long snq, @AuthenticationPrincipal LogInReqDTO customer) {
+    public String addCart(@RequestBody Long snq, @AuthenticationPrincipal LogInReqDTO customer) {
         String email = customer.getEmail();
         return cartService.addCart(snq, email);
     }
     @DeleteMapping("/cart")
-    public String deleteProduct(@AuthenticationPrincipal LogInReqDTO customer, @PathVariable("snq") Long id){
+    public String deleteProduct(@AuthenticationPrincipal LogInReqDTO customer, @RequestBody Long id){
         String email = customer.getEmail();
         return cartService.deleteProduct(email, id);
     }
