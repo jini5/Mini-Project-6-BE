2b5336f7e97079688be5d243b96d766006c5ac0d
Blooossom <taegyeong19@gmail.com>
2023-02-16 13:55:53 +0900

    #35 Service/Repository 누락 Annotation 추가

--- src/main/java/com/mini/money/repository/CartRepository.java
+++ src/main/java/com/mini/money/repository/CartRepository.java
@@ -2,7 +2,9 @@ package com.mini.money.repository;
 
 import com.mini.money.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
 }
