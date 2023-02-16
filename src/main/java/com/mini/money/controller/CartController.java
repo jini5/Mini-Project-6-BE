6f5ca97cd17f441dac415e70a60d702cbf79c5fb
Blooossom <taegyeong19@gmail.com>
2023-02-16 14:19:02 +0900

    #36 Controller 내 deleteProduct 추가
    1. snq, email 입력 받아 Service에 전달

--- src/main/java/com/mini/money/controller/CartController.java
+++ src/main/java/com/mini/money/controller/CartController.java
@@ -4,6 +4,7 @@ import com.mini.money.entity.Customer;
 import com.mini.money.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +20,8 @@ public class CartController {
         String email = customer.getEmail();
         return cartService.addCart(snq, email);
     }
-
+    @DeleteMapping("/cart")
+    public String deleteProduct(String email, @PathVariable("snq") Long id){
+        return cartService.deleteProduct(email, id);
+    }
 }
