761f607735eb27e62c5b390c7e98e5422e5ac8ad
jini5 <lim789789@gmail.com>
2023-02-16 13:42:29 +0900

    feat: SIGNUP Controller 작성

--- src/main/java/com/mini/money/controller/CustomerController.java
+++ src/main/java/com/mini/money/controller/CustomerController.java
@@ -1,13 +1,16 @@
 package com.mini.money.controller;
 
+import com.mini.money.dto.CustomerReqDTO;
 import com.mini.money.dto.myinfo.MyCustomerInfoResDTO;
 import com.mini.money.entity.Customer;
+import com.mini.money.service.AuthService;
 import com.mini.money.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
 
     private final CustomerService customerService;
+    private final AuthService authService;
 
     @GetMapping("/mypage/info")
     public ResponseEntity<MyCustomerInfoResDTO> findMyInfo(@AuthenticationPrincipal Customer customer) {
@@ -23,4 +27,11 @@ public class CustomerController {
 
         return new ResponseEntity<>(myCustomerInfoResDTO, HttpStatus.OK);
     }
+
+    @PostMapping("/signup")
+    public String signUp(CustomerReqDTO signupReqDTO){
+        return authService.signup(signupReqDTO);
+    }
+
+
 }
