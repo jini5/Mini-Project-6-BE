86797fb6f2d156c68c5bd25311e6a40e065f47ba
jini5 <lim789789@gmail.com>
2023-02-16 14:18:49 +0900

    feat: logout기능 구현

--- src/main/java/com/mini/money/controller/CustomerController.java
+++ src/main/java/com/mini/money/controller/CustomerController.java
@@ -8,12 +8,14 @@ import com.mini.money.entity.Customer;
 import com.mini.money.repository.TokenRepository;
 import com.mini.money.service.AuthService;
 import com.mini.money.service.CustomerService;
+import com.mini.money.service.TokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,6 +24,7 @@ public class CustomerController {
 
     private final CustomerService customerService;
     private final AuthService authService;
+    private final TokenService tokenService;
 
     @GetMapping("/mypage/info")
     public ResponseEntity<MyCustomerInfoResDTO> findMyInfo(@AuthenticationPrincipal Customer customer) {
@@ -40,5 +43,10 @@ public class CustomerController {
         return  authService.login(logInReqDTO);
     }
 
+    @PostMapping("/logout")
+    public String logOut(@RequestHeader(name = "Authorization") String token){
+        tokenService.logout(token);
+        return "success";
+    }
 
 }
