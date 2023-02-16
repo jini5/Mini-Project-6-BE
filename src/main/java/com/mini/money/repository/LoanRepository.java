e55d22cc936e8a30c8f09a8b5fc79b82b7e0d619
jini5 <lim789789@gmail.com>
2023-02-17 04:18:03 +0900

    feat: LoanRepository 추가 1. 입력받은 area이 포함된 대출 상품을 찾는다.

--- src/main/java/com/mini/money/repository/LoanRepository.java
+++ src/main/java/com/mini/money/repository/LoanRepository.java
@@ -4,6 +4,7 @@ import com.mini.money.entity.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +12,6 @@ public interface LoanRepository extends JpaRepository<Loan, Long> {
 
     Optional<Loan> findBySnq(Long snq);
 
+
+    List<Loan> findAllByAreaContaining(String area);
 }
