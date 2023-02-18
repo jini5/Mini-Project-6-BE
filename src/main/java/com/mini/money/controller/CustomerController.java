b70254004f9c2f3ae41f21cef3a60e7da4ab7a20
DongHyos <dong87070@naver.com>
2023-02-18 15:39:04 +0900

    #62 - 컨트롤러 추가
    HttpStatus.Ok 와 함께 success 메세지 전달

--- src/main/java/com/mini/money/controller/CustomerController.java
+++ src/main/java/com/mini/money/controller/CustomerController.java
@@ -4,8 +4,7 @@ import com.mini.money.dto.CustomerReqDTO;
 import com.mini.money.dto.LogInReqDTO;
 import com.mini.money.dto.LogInResDTO;
 import com.mini.money.dto.myinfo.MyCustomerInfoResDTO;
-import com.mini.money.entity.Customer;
-import com.mini.money.repository.TokenRepository;
+import com.mini.money.dto.myinfo.UpdateInfoReqDTO;
 import com.mini.money.service.AuthService;
 import com.mini.money.service.CustomerService;
 import com.mini.money.service.TokenService;
@@ -32,18 +31,25 @@ public class CustomerController {
     }
 
     @PostMapping("/signup")
-    public String signUp(@RequestBody CustomerReqDTO signupReqDTO){
+    public String signUp(@RequestBody CustomerReqDTO signupReqDTO) {
         return authService.signup(signupReqDTO);
     }
+
     @PostMapping("/login")
-    public LogInResDTO logIn(@RequestBody LogInReqDTO logInReqDTO){
-        return  authService.login(logInReqDTO);
+    public LogInResDTO logIn(@RequestBody LogInReqDTO logInReqDTO) {
+        return authService.login(logInReqDTO);
     }
 
     @PostMapping("/logout")
-    public String logOut(@RequestHeader(name = "Authorization") String token){
+    public String logOut(@RequestHeader(name = "Authorization") String token) {
         tokenService.logout(token);
         return "success";
     }
 
+    @PutMapping("/mypage/member")
+    public ResponseEntity<String> updateInfo(@RequestBody UpdateInfoReqDTO updateInfoReqDTO, @AuthenticationPrincipal LogInReqDTO customer) {
+        String email = customer.getEmail();
+        String message = authService.updateInfo(updateInfoReqDTO, email);
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
 }
