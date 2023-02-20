ceb4130e49da748e5e241c5970c5ab4986819c8f
DongHyos <dong87070@naver.com>
2023-02-20 15:17:42 +0900

    Auto stash before merge of "develop" and "origin/develop"

---
+++ src/test/java/com/mini/money/repository/CustomerDetailRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.mini.money.repository;
+
+import com.mini.money.entity.Customer;
+import com.mini.money.entity.CustomerDetail;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CustomerDetailRepositoryTest {
+
+    @Autowired
+    CustomerDetailRepository customerDetailRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    @Transactional
+    void name() {
+        String email = "dong8707@naver.com";
+        Customer customer = customerRepository.findByEmail(email);
+//        CustomerDetail customerDetail = customerDetailRepository.findByCustomer(customer);
+
+//        System.out.println(customerDetail.getCustomer().getEmail());
+    }
+}
---
+++ src/test/java/com/mini/money/repository/FavorRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.mini.money.repository;
+
+import com.mini.money.dto.LoanResDTO;
+import com.mini.money.entity.Customer;
+import com.mini.money.entity.Favor;
+import com.mini.money.entity.Loan;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class FavorRepositoryTest {
+
+
+}
