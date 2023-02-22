d3e2f0af5d4bfe0ac1e23a4934dc8a28eaf30a9e
Blooossom <taegyeong19@gmail.com>
2023-02-22 23:11:13 +0900

    #82 CustomerDetailRepository 로직 추가
    상세정보 수정 로직 추가

--- src/main/java/com/mini/money/repository/CustomerDetailRepository.java
+++ src/main/java/com/mini/money/repository/CustomerDetailRepository.java
@@ -3,10 +3,19 @@ package com.mini.money.repository;
 import com.mini.money.entity.Customer;
 import com.mini.money.entity.CustomerDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 
 import java.util.Optional;
 
 public interface CustomerDetailRepository extends JpaRepository<CustomerDetail, Long> {
 
     Optional<CustomerDetail> findByCustomer(Customer customer);
+
+
+    @Modifying
+    @Query(value = "UPDATE customer_detail c SET c.address = :address, c.age = :age, c.crdt_Grade = :crdtGrade, c.job = :job, c.income = :income, c.bank = :bank WHERE c.detail_id = :id", nativeQuery = true)
+    Integer updateDetailInfo(@Param("address") String address, @Param("age") Integer age, @Param("crdtGrade") Double crdtGrade, @Param("job") String job, @Param("income")Integer income, @Param("bank") String bank, @Param("id") Long id);
 }
